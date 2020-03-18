d5415f8166aa76221ade5c0ac467122ef2789322
aleurdo <7777846@gmail.com>
2020-03-18 17:12:42 +0300

    init: task human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    var converter = require('number-to-words');
+    var words = converter.toWords(number);
+    return words.replace('-', ' ');
+
+
 }
